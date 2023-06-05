551ce6ed3377c978bdea2a675d6ffa6509aa664c
Ying <43379928+yingshiuan@users.noreply.github.com>
2023-06-05 15:59:15 +0800

    Update ga.js

--- static/js/ga.js
+++ static/js/ga.js
@@ -1,9 +1,7 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-4JY7EDJ3QJ"></script>
-<script>
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-4JY7EDJ3QJ');
-</script>
